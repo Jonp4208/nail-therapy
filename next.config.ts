981e087b0b9c87d168f60b012894216ddec673e5
Jonathon <jonp4208@gmail.com>
2025-04-24 16:42:31 -0400

    Update Next.js image configuration to allow all image domains

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,13 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/**',
+      },
     ],
+    domains: ['images.unsplash.com', 'rzezoxuqhavwwvwfozip.supabase.co'],
   },
 };
 
